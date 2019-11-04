f32035367a7dbb6fa7640ac2f17ecb086b86b4cd
Pranay Pathole <38600100+PPathole@users.noreply.github.com>
2019-11-04 22:40:34 +0530

    Update sum.cpp

--- HPC/Reduction/sum.cpp
+++ HPC/Reduction/sum.cpp
@@ -2,24 +2,20 @@
 #include<omp.h>
 #include<time.h>
 using namespace std;
-#define n 10
+#define n 20
 
 int main(){
-	int a[n], b[n], i, sum=0;
-
-	for(i=0; i<n; i++){
-		a[i] = rand() % 10;
-	}
-
-	for(i=0; i<n; i++){
-		b[i] = rand() % 10;
-	}
-
-	#pragma omp parallel for reduction(+:sum)
-	for(i=0; i<n; i++){
-		sum = sum + (a[i] * b[i]);
-	}
-
-	cout<<"The sum is: "<<sum;
-	return 0;
+    int arr[n], sum=0;
+    #pragma omp parallel for 
+    for(int i=0; i<n; i++){
+        arr[i] = i;
+    }
+    
+    #pragma omp parallel for reduction(+:sum)
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+    
+    cout<<"Sum: "<<sum;
+    return 0;
 }
